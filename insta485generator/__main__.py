cf0c434c6f132a3f03e99ff3df04f5c7339f7557
Qifa Wang <qifwang@umich.edu>
2022-01-17 13:17:24 -0500

    Final version

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -20,14 +20,14 @@ def main(input_dir, verbose, output):
         sys.exit(f"\'{input_dir}\' is not a directory.")
 
     if output == "html":
-        generated_dest = f"{input_dir}/html"
+        generated_dest = f"{input_dir}/{output}"
     else:
         generated_dest = f"{output}"
 
     if os.path.exists(f"{input_dir}/static"):
         static_files = os.listdir(f"{input_dir}/static")
         for static_file in static_files:
-            full_file_name = os.path.join(f"{input_dir}/static")
+            full_file_name = os.path.join(f"{input_dir}/static", static_file)
             shutil.copytree(full_file_name,
                             f"{generated_dest}/{static_file}")
     else:
@@ -36,25 +36,29 @@ def main(input_dir, verbose, output):
     if verbose:
         print(f"Rendered index.html -> {generated_dest}/index.html")
 
-    with open(f"{input_dir}/config.json") as js:
-        data = json.load(js)
+    with open(f"{input_dir}/config.json",
+              "r",
+              encoding="utf-8") as in_file:
+        data = json.load(in_file)
 
-        for d in data:
-            url = d["url"]
+        for info in data:
+            url = info["url"]
 
             env = jinja2.Environment(loader=jinja2.FileSystemLoader(
                 f"{input_dir}/templates/"),
                 autoescape=jinja2.
                 select_autoescape(["html", "xml"]))
 
-            template = env.get_template(d["template"])
-            generated = template.render(d["context"])
+            template = env.get_template(info["template"])
+            generated = template.render(info["context"])
 
             if not os.path.exists(f"{generated_dest}{url}"):
                 os.makedirs(f"{generated_dest}{url}")
 
-            with open(f"{generated_dest}{url}/index.html", "w") as f:
-                f.write(generated)
+            with open(f"{generated_dest}{url}/index.html",
+                      "w",
+                      encoding="utf-8") as out_file:
+                out_file.write(generated)
 
 
 if __name__ == '__main__':
