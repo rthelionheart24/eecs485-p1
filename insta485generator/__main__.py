b844b214fb6c667276e367772fbbf72fa5353960
Qifa Wang <qifwang@umich.edu>
2022-01-24 18:52:17 -0500

    Final fix

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -19,10 +19,15 @@ def main(input_dir, verbose, output):
     if not os.path.exists(input_dir):
         sys.exit(f"\'{input_dir}\' is not a directory.")
 
+    generated_dest = f"{output}"
+
     if output == "html":
         generated_dest = f"{input_dir}/{output}"
-    else:
-        generated_dest = f"{output}"
+
+    if os.path.exists(generated_dest):
+        sys.exit("Output directory already exists")
+
+    os.makedirs(generated_dest)
 
     if os.path.exists(f"{input_dir}/static"):
         static_files = os.listdir(f"{input_dir}/static")
@@ -30,35 +35,43 @@ def main(input_dir, verbose, output):
             full_file_name = os.path.join(f"{input_dir}/static", static_file)
             shutil.copytree(full_file_name,
                             f"{generated_dest}/{static_file}")
-    else:
-        os.makedirs(generated_dest)
 
     if verbose:
         print(f"Rendered index.html -> {generated_dest}/index.html")
 
-    with open(f"{input_dir}/config.json",
-              "r",
-              encoding="utf-8") as in_file:
-        data = json.load(in_file)
+    try:
+        with open(f"{input_dir}/config.json",
+                  "r",
+                  encoding="utf-8") as in_file:
+            data = json.load(in_file)
 
-        for info in data:
-            url = info["url"]
+            for info in data:
+                url = info["url"]
 
-            env = jinja2.Environment(loader=jinja2.FileSystemLoader(
-                f"{input_dir}/templates/"),
-                autoescape=jinja2.
-                select_autoescape(["html", "xml"]))
+                env = jinja2.Environment(loader=jinja2.FileSystemLoader(
+                    f"{input_dir}/templates/"),
+                    autoescape=jinja2.
+                    select_autoescape(["html", "xml"]))
 
-            template = env.get_template(info["template"])
-            generated = template.render(info["context"])
+                template = env.get_template(info["template"])
+                generated = template.render(info["context"])
 
-            if not os.path.exists(f"{generated_dest}{url}"):
-                os.makedirs(f"{generated_dest}{url}")
+                if not os.path.exists(f"{generated_dest}{url}"):
+                    os.makedirs(f"{generated_dest}{url}")
 
-            with open(f"{generated_dest}{url}/index.html",
-                      "w",
-                      encoding="utf-8") as out_file:
-                out_file.write(generated)
+                with open(f"{generated_dest}{url}/index.html",
+                          "w",
+                          encoding="utf-8") as out_file:
+                    out_file.write(generated)
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+    except jinja2.TemplateError:
+        print("Jinja2 error")
+        sys.exit(2)
+    except json.JSONDecodeError:
+        print("Json error")
+        sys.exit(3)
 
 
 if __name__ == '__main__':
